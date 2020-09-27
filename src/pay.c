a40c192086cf5641f2441f0a63d9cf2ece69dd92
99002479 <71481149+99002479@users.noreply.github.com>
2020-09-27 16:00:15 +0530

    Update pay.c

--- src/pay.c
+++ src/pay.c
@@ -1,27 +1,5 @@
 #include <pay.h>
 
-struct date{
-	   int month;
-	   int day;
-	   int year;
-	   };
-
-struct account {
-	int number;
-	char name[100];
-	int acct_no;
-	float mobile_no;
-	char street[100];
-	char city[100];
-	char acct_type;
-	float oldbalance;
-	float newbalance;
-	float payment;
-	struct date lastpayment;
-  }customer;
-  int tl,sl,ts;
-
- 
 int checkbalance(float old, float pay)
 {
     return (old-pay);

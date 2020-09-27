9a1a1332b21fafd8c0c1edccf910a88a0f328d2e
99002479 <71481149+99002479@users.noreply.github.com>
2020-09-27 19:29:42 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,25 +1,5 @@
 #include <pay.h>			/* Add include file*/
 
-struct date{
-	   int month;
-	   int day;
-	   int year;
-	   };
-
-struct account {
-	int number;
-	char name[100];
-	int acct_no;
-	float mobile_no;
-	char street[100];
-	char city[100];
-	char acct_type;
-	float oldbalance;
-	float newbalance;
-	float payment;
-	struct date lastpayment;
-  }customer;
-
 void main()
 	{
 	  int i,n;

ff4482a9b0b33c2de422da78fb325a3efac3f6d8
99002479 <71481149+99002479@users.noreply.github.com>
2020-09-27 17:41:26 +0530

    Update test_pay.c

--- test/test_pay.c
+++ test/test_pay.c
@@ -1,40 +1,53 @@
-/*
 #include <CUnit/Basic.h>
 #include <CUnit/CUnit.h>
-#include <pay.h>
 
+#include <pay.h>
+#include <string.h>
 #define PROJECT_NAME "Payment"
 
-	void setUp()
-	{	}
-	void tearDown()
-	{	}
-	
-	void test_balance(void)
-	{
-  		TEST_ASSERT_EQUAL(950, checkbalance(1000, 50));
-  		TEST_ASSERT_EQUAL(-50, checkbalance(100, 150));
-		TEST_ASSERT_EQUAL(-150, checkbalance(-50, 100));
-		TEST_ASSERT_EQUAL(-50, checkbalance(-50, 0));
-		TEST_ASSERT_EQUAL(100, checkbalance(100, 0));
-	}
-
-	void test_accounttype(void)
-	{
-  		TEST_ASSERT_EQUAL(1, getaccounttype(1000, 50));
-  		TEST_ASSERT_EQUAL(0, getaccounttype(100, 150));
-		TEST_ASSERT_EQUAL(1, getaccounttype(100, 0));
-		TEST_ASSERT_EQUAL(-1, getaccounttype(-100, 0));
-	}
+/* Prototypes for all the test functions */
+void test_balance(void)
+void test_accounttype(void);
 
 
-	int main()
-	{
-	  	UNITY_BEGIN();
-  	
-	  	RUN_TEST(test_balance);
-		
-		RUN_TEST(test_accounttype);
+/* Start of the application test */
+int main() {
+/* Note: Do not edit START*/
+  /*Initialize and setup the Test Framework */
+  if (CUE_SUCCESS != CU_initialize_registry())
+    return CU_get_error();
+  CU_pSuite suite = CU_add_suite(PROJECT_NAME, 0, 0);
+/* Note: Do not edit END */
 
-  		return UNITY_END();
-	}
+/* Add your test functions in this format for testing*/
+  CU_add_test(suite, "checkbalance", test_balance);
+  CU_add_test(suite, "getaccounttype", test_accounttype);
+
+
+/* Note: Do not edit START*/
+  /* Setup Test Framework to output the result to Screen */
+  CU_basic_set_mode(CU_BRM_VERBOSE);
+
+  /* run the unit test framework*/
+  CU_basic_run_tests();
+
+  /* Cleaning the Resources used by Unit test framework */
+  CU_cleanup_registry();
+/* Note: Do not edit END */
+  return 0;
+}
+/* Write all the test functions */
+void test_balance(void) {
+  CU_ASSERT(950 == checkbalance(1000, 50));
+
+  /* Dummy fail*/
+  CU_ASSERT(100 == checkbalance(100, 10));
+}
+
+void test_accounttype(void) {
+  CU_ASSERT(1 == getaccounttype(1000, 50));
+
+  /* Dummy fail*/
+  CU_ASSERT(0 == getaccounttype(1000, 50));
+}
+
